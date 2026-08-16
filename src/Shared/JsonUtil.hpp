b96869253078a8ed23fd2b9f39f4a5b8be9856ae
SameForYou <amardabest375@gmail.com>
2026-08-16 16:52:26 +0200

    Organize json api

---
+++ src/Shared/JsonUtil.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <nlohmann/json.hpp>
+
+#include "Types.hpp"
+
+namespace ssg::json
+{
+using nlohmann::json;
+
+template <typename TExpected>
+TExpected AttemptAccessField(const nlohmann::json& data, std::string_view field)
+{
+    auto it = data.find(field);
+    if (it == data.end())
+    {
+        std::string id = data.value("id", "UNKNOWN_ENTITY");
+        throw std::runtime_error("Missing required field in JSON for entity");
+    }
+
+    try
+    {
+        return it->get<TExpected>();
+    }
+    catch (const nlohmann::json::exception& e)
+    {
+        // Catch type mismatch errors (e.g., field exists, but tried to read a string as int)
+        throw std::runtime_error(e.what());
+    }
+}
+
+const nlohmann::json& AccessObjectField(const nlohmann::json& data, std::string_view field);
+
+ssg::Vec2 ReadVec2(const nlohmann::json& data, std::string_view field, std::string_view x = "x",
+                   std::string_view y = "y");
+
+ssg::Vec3 ReadVec3(const nlohmann::json& data, std::string_view field, std::string_view x = "x",
+                   std::string_view y = "y", std::string_view z = "z");
+
+ssg::Vec4 ReadVec4(const nlohmann::json& data, std::string_view field, std::string_view x = "x",
+                   std::string_view y = "y", std::string_view z = "z", std::string_view w = "w");
+} // namespace ssg::json
