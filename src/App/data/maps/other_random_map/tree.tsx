c6fe825b8c04cad51d8812054501502d6229653d
sameforyou <amardabest375@gmail.com>
2026-09-11 18:11:49 +0200

    refactor: migrate tsx to tsj

--- src/App/data/maps/other_random_map/tree.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.1" name="tree" tilewidth="32" tileheight="32" tilecount="9" columns="3">
- <image source="assets/Textures/tree.png" width="100" height="100"/>
-</tileset>
